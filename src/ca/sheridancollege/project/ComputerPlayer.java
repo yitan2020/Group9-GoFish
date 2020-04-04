a39a4c271813d863fccee4eb5c4469aea2db2f5f
yitan2020 <tan5@sheridancollege.ca>
2020-04-04 09:18:48 -0400

    add data fields and methods to ComputerPlayer class

---
+++ src/ca/sheridancollege/project/ComputerPlayer.java
@@ -0,0 +1,15 @@
+package ca.sheridancollege.project;
+
+public class ComputerPlayer extends Player {
+
+    public ComputerPlayer(String playerID) {
+        super(playerID);
+    }
+    
+    @Override
+    public void play() {
+        
+        
+    }
+
+}
