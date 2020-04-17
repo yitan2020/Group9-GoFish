ccbfb2d55aa68422e5a3b272386a7ff085733d69
yitan2020 <tan5@sheridancollege.ca>
2020-04-17 11:40:55 -0400

    Finish the menu method in the GameInterface class

--- src/ca/sheridancollege/project/GameInterface.java
+++ src/ca/sheridancollege/project/GameInterface.java
@@ -25,13 +25,8 @@ public class GameInterface {
         
         if(player1 instanceof UserPlayer) {
             UserPlayer user1 = (UserPlayer) player1;
-            if(user1.play()) {
-                user1.setNumOfWin(1);
-            } else {
-                user1.setNumOfLos(1);
-            }
-            
-           user1.displayResult();
+            boolean flag = true;
+            Menu(flag, user1);
         }
         
      
@@ -40,6 +35,32 @@ public class GameInterface {
 
         
 
+    }
+    
+    public static void Menu(boolean flag, UserPlayer user1) {
+        // interact with user to start the game or leave the game
+            do{
+                System.out.println("Press Y to start the game or N to end the game");
+                Scanner input = new Scanner (System.in);
+                String str = input.nextLine();
+                switch (str) {
+                        case "Y": 
+                            if(user1.play()) {
+                                user1.setNumOfWin(1);
+                            } else {
+                                user1.setNumOfLos(1);
+                            }
+                            user1.displayResult();
+                            break;
+                        case "N": 
+                            System.out.println("see you next time!");
+                            flag=false;
+                            break;
+                        default:
+                            System.out.println("input is invalid. please re-enter");
+                            break;
+                }    
+            } while(flag);
     }
 
 
